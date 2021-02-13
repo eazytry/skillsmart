96b57ffb17664f57d608c57f2ee9196988df4f62
Фомичев Алексей Геннадьевич (4042333) <agfomichev@dev.vtb>
2021-02-13 03:28:58 +0300

    stack task and tests

--- src/main/java/dataStructures/stack/Stack.java
+++ src/main/java/dataStructures/stack/Stack.java
@@ -1,4 +1,67 @@
-package dataStructures.queue;
+package dataStructures.stack;
 
-public class Queue {
+public class Stack<T> {
+    public Node<T> head;
+    public Node<T> tail;
+    public int count;
+
+    public Stack() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public T pop() {
+        if (count == 0)
+            return null;
+        Node<T> valNode = this.head;
+        if (count == 1) {
+            this.head = null;
+            this.tail = null;
+        }
+        T val = valNode.getT();
+        this.head = valNode.nextNode;
+        count--;
+        return val;
+    }
+
+    public void push(T val) {
+        Node<T> node = new Node<>(val);
+        if (count == 0)
+            this.head = node;
+        else if (count == 1) {
+            this.head.nextNode = node;
+        }
+        else {
+            this.tail.nextNode = node;
+        }
+        this.tail = node;
+        count++;
+    }
+
+    public T peek() {
+        return head == null ? null : head.getT();
+    }
+
+    public static class Node<T> {
+        public Node<T> nextNode;
+        public T t;
+
+        public Node(T t) {
+            this.nextNode = null;
+            this.t = t;
+        }
+
+        public void setNextNode(Node<T> nextNode) {
+            this.nextNode = nextNode;
+        }
+
+        public T getT() {
+            return t;
+        }
+    }
 }
--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,2 +1,155 @@
-package PACKAGE_NAME;public class StackTest {
+import dataStructures.stack.Stack;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class StackTest {
+    @Test
+    public void init() {
+        Stack<String> stringStack = new Stack<>();
+
+        assertEquals(0, stringStack.size());
+        assertNull(stringStack.peek());
+        assertNull(stringStack.peek());
+    }
+
+    @Test
+    public void push_cashIsEmpty() {
+        Stack<String> stringStack = new Stack<>();
+        String expectedString = "123";
+
+        stringStack.push(expectedString);
+
+        assertEquals(1, stringStack.size());
+        assertEquals(expectedString, stringStack.head.getT());
+        assertEquals(stringStack.head, stringStack.tail);
+        assertEquals(stringStack.head.t, stringStack.tail.t);
+        assertNull(stringStack.tail.nextNode);
+    }
+
+    @Test
+    public void push_2Elements() {
+        Stack<String> stringStack = new Stack<>();
+        String firstString = "123";
+        String secondString = "321";
+
+        stringStack.push(firstString);
+        stringStack.push(secondString);
+
+        assertEquals(2, stringStack.size());
+        assertEquals(firstString, stringStack.head.t);
+        assertEquals(secondString, stringStack.tail.t);
+        assertEquals(stringStack.head.nextNode, stringStack.tail);
+        assertNull(stringStack.tail.nextNode);
+    }
+
+    @Test
+    public void push_3Elements() {
+        Stack<String> stringStack = new Stack<>();
+        String firstString = "123";
+        String secondString = "321";
+        String thirdString = "000";
+
+        stringStack.push(firstString);
+        stringStack.push(secondString);
+        stringStack.push(thirdString);
+
+        assertEquals(3, stringStack.size());
+        assertEquals(firstString, stringStack.head.t);
+        assertEquals(thirdString, stringStack.tail.t);
+        assertEquals(stringStack.head.nextNode.t, secondString);
+        assertNull(stringStack.tail.nextNode);
+    }
+
+    @Test
+    public void pop_stackIsEmpty() {
+        Stack<String> stringStack = new Stack<>();
+
+        String val = stringStack.pop();
+
+        assertEquals(0, stringStack.size());
+        assertNull(val);
+        assertNull(stringStack.head);
+        assertNull(stringStack.tail);
+    }
+
+    @Test
+    public void pop_countIsOne() {
+        Stack<String> stringStack = new Stack<>();
+
+        String expectedVal = "11";
+        stringStack.push(expectedVal);
+        String actualVal = stringStack.pop();
+
+        assertEquals(0, stringStack.size());
+        assertEquals(expectedVal, actualVal);
+        assertNull(stringStack.head);
+        assertNull(stringStack.tail);
+    }
+
+    @Test
+    public void pop_countIsTwo() {
+        Stack<String> stringStack = new Stack<>();
+
+        String firstVal = "11";
+        String secondVal = "12";
+        stringStack.push(firstVal);
+        stringStack.push(secondVal);
+        String actualVal = stringStack.pop();
+
+        assertEquals(1, stringStack.size());
+        assertEquals(firstVal, actualVal);
+        assertEquals(secondVal, stringStack.head.getT());
+        assertEquals(secondVal, stringStack.tail.getT());
+    }
+
+    @Test
+    public void pop_countIsThree() {
+        Stack<String> stringStack = new Stack<>();
+
+        String firstVal = "11";
+        String secondVal = "12";
+        String thirdVal = "12";
+        stringStack.push(firstVal);
+        stringStack.push(secondVal);
+        stringStack.push(thirdVal);
+        String actualVal = stringStack.pop();
+
+        assertEquals(2, stringStack.size());
+        assertEquals(firstVal, actualVal);
+        assertEquals(secondVal, stringStack.head.getT());
+        assertEquals(thirdVal, stringStack.tail.getT());
+        assertEquals(stringStack.head.nextNode, stringStack.tail);
+        assertNull(stringStack.tail.nextNode);
+    }
+
+    @Test
+    public void peek_stackIsEmpty() {
+        Stack<String> stringStack = new Stack<>();
+
+        String actualVal = stringStack.peek();
+
+        assertNull(actualVal);
+    }
+
+    @Test
+    public void peek_stackIsNotEmpty() {
+        Stack<String> stringStack = new Stack<>();
+
+        String expectedVal = "123";
+
+        stringStack.push(expectedVal);
+        stringStack.push("fff");
+        stringStack.push("432432");
+
+        String actualVal = "";
+
+        for (int i = 0; i < 10; i++) {
+            actualVal = stringStack.peek();
+        }
+
+        assertEquals(expectedVal, actualVal);
+        assertEquals(3, stringStack.size());
+    }
 }
