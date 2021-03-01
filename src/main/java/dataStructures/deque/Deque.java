f2a4fd6b7d3cf7048ade0e5c90fe219fceb1b56f
Фомичев Алексей Геннадьевич (4042333) <agfomichev@dev.vtb>
2021-03-01 09:40:11 +0300

    add deque and tests

---
+++ src/main/java/dataStructures/deque/Deque.java
@@ -0,0 +1,87 @@
+package dataStructures.deque;
+
+public class Deque<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public Deque() {
+        this.head = null;
+        this.head = null;
+        this.size = 0;
+        // инициализация внутреннего хранилища
+    }
+
+    public void addFront(T item) {
+        Node<T> node = new Node<>(item);
+        if (size == 0)
+            initDeque(node);
+        else {
+            node.nextNode = this.head;
+            this.head.prevNode = node;
+            this.head = node;
+        }
+        size++;
+        // добавление в голову
+    }
+
+    public void addTail(T item) {
+        Node<T> node = new Node<>(item);
+        if (size == 0)
+            initDeque(node);
+        else {
+            node.prevNode = this.tail;
+            this.tail.nextNode = node;
+            this.tail = node;
+        }
+        size++;
+        // добавление в хвост
+    }
+
+    public T removeFront() {
+        if (isEmpty())
+            return null;
+        Node<T> removingValue = this.head;
+        if (removingValue.nextNode != null)
+            removingValue.nextNode.prevNode = null;
+        this.head = removingValue.nextNode;
+        size--;
+        return removingValue.value;
+    }
+
+    public T removeTail() {
+        if (isEmpty())
+            return null;
+        Node<T> removingValue = this.tail;
+        if (removingValue.prevNode != null)
+            removingValue.prevNode.nextNode = null;
+        this.tail = removingValue.prevNode;
+        size--;
+        return removingValue.value;
+        // удаление из хвоста
+    }
+
+    public int size() {
+        return size; // размер очереди
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void initDeque(Node<T> firstNode) {
+        this.head = firstNode;
+        this.tail = firstNode;
+    }
+
+
+    private static class Node<T> {
+        private Node<T> prevNode;
+        private Node<T> nextNode;
+        private T value;
+
+        Node(T value) {
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/DequeueTest.java
@@ -0,0 +1,117 @@
+import dataStructures.deque.Deque;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class DequeueTest {
+    @Test
+    public void initDeque() {
+        Deque<String> deque = new Deque<>();
+
+        Assertions.assertThat(deque.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void given_EmptyDeque_when_AddFrontAndRemoveFront_than_RemoveElementIsSameAsAdd() {
+        Deque<String> deque = new Deque<>();
+
+        String expectedValue = "123";
+
+        deque.addFront(expectedValue);
+
+        Assertions.assertThat(deque.size()).isEqualTo(1);
+
+        String actualValue = deque.removeFront();
+
+        Assertions.assertThat(actualValue).isEqualTo(expectedValue);
+        Assertions.assertThat(deque.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void given_FilledDeque_when_RemoveFront_than_RemovingOrderIsCorrect() {
+        // given
+        String firstExpectedValue = "1";
+        String secondExpectedValue = "2";
+        String thirdExpectedValue = "3";
+        Deque<String> deque = new Deque<>();
+        deque.addFront(firstExpectedValue);
+        deque.addFront(secondExpectedValue);
+        deque.addFront(thirdExpectedValue);
+        Assertions.assertThat(deque.size()).isEqualTo(3);
+
+        //when
+        String firstActualValue = deque.removeFront();
+        String secondActualValue = deque.removeFront();
+        String thirdActualValue = deque.removeFront();
+        Assertions.assertThat(deque.size()).isEqualTo(0);
+
+        //than
+        Assertions.assertThat(firstActualValue).isEqualTo(thirdExpectedValue);
+        Assertions.assertThat(secondActualValue).isEqualTo(secondExpectedValue);
+        Assertions.assertThat(thirdActualValue).isEqualTo(firstExpectedValue);
+    }
+
+    @Test
+    public void given_EmptyDeque_when_AddTailRemoveTail_than_RemoveValueIsSameAsAdd() {
+        //given
+        String expectedValue = "123";
+        Deque<String> deque = new Deque<>();
+
+        //when
+        deque.addTail(expectedValue);
+        String actualValue = deque.removeTail();
+
+        //than
+        Assertions.assertThat(actualValue).isEqualTo(expectedValue);
+        Assertions.assertThat(deque.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void given_FilledDeque_when_RemoveTail_than_RemovingOrderIsCorrect() {
+        // given
+        String firstExpectedValue = "1";
+        String secondExpectedValue = "2";
+        String thirdExpectedValue = "3";
+        Deque<String> deque = new Deque<>();
+        deque.addTail(firstExpectedValue);
+        deque.addTail(secondExpectedValue);
+        deque.addTail(thirdExpectedValue);
+        Assertions.assertThat(deque.size()).isEqualTo(3);
+
+        //when
+        String firstActualValue = deque.removeTail();
+        String secondActualValue = deque.removeTail();
+        String thirdActualValue = deque.removeTail();
+        Assertions.assertThat(deque.size()).isEqualTo(0);
+
+        //than
+        Assertions.assertThat(firstActualValue).isEqualTo(thirdExpectedValue);
+        Assertions.assertThat(secondActualValue).isEqualTo(secondExpectedValue);
+        Assertions.assertThat(thirdActualValue).isEqualTo(firstExpectedValue);
+    }
+
+    @Test
+    public void given_EmptyDeque_when_AddFrontAndTail_than_RemovingFromFrondAndTailIsCorrect() {
+        //given
+        Deque<String> deque = new Deque<>();
+        String firstExpectedValue = "1";
+        String secondExpectedValue = "2";
+        String thirdExpectedValue = "3";
+        String fourthExpectedValue = "4";
+
+        //when
+        deque.addTail(firstExpectedValue);
+        deque.addFront(secondExpectedValue);
+        deque.addTail(thirdExpectedValue);
+        deque.addFront(fourthExpectedValue);
+
+        //than
+        String firstActualValue = deque.removeFront();
+        String secondActualValue = deque.removeTail();
+        String thirdActualValue = deque.removeFront();
+        String fourthActualValue = deque.removeTail();
+        Assertions.assertThat(firstActualValue).isEqualTo(fourthExpectedValue);
+        Assertions.assertThat(secondActualValue).isEqualTo(thirdExpectedValue);
+        Assertions.assertThat(thirdActualValue).isEqualTo(secondExpectedValue);
+        Assertions.assertThat(fourthActualValue).isEqualTo(firstExpectedValue);
+    }
+}
