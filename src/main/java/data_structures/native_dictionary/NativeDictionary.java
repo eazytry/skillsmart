613b0d8ea69e8e16074654c8ae1e06b16756abea
Фомичев Алексей Геннадьевич (4042333) <agfomichev@dev.vtb>
2021-03-15 13:25:07 +0300

    fix & add tests

--- src/main/java/data_structures/native_dictionary/NativeDictionary.java
+++ src/main/java/data_structures/native_dictionary/NativeDictionary.java
@@ -1,8 +1,12 @@
 package data_structures.native_dictionary;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
 
-class NativeDictionary<T> {
+public class NativeDictionary<T> {
     public int size;
     public String[] slots;
     public T[] values;
@@ -21,20 +25,49 @@ class NativeDictionary<T> {
     public boolean isKey(String key) {
         // возвращает true если ключ имеется,
         // иначе false
-        return slots[hashFun(key)] != null;
+        return Arrays.stream(getKeySearchIndexSequence(key, hashFun(key)))
+                .filter(i -> slots[i] != null && slots[i].equals(key))
+                .findFirst()
+                .isPresent();
     }
 
     public void put(String key, T value) {
         // гарантированно записываем
         // значение value по ключу key
-        int index = hashFun(key);
-        slots[index] = key;
-        values[index] = value;
+        Arrays.stream(getKeySearchIndexSequence(key, hashFun(key)))
+                .filter(i -> slots[i] == null)
+                .findFirst()
+                .ifPresent(new PutConsumer(slots, values, key, value));
     }
 
     public T get(String key) {
-        if (isKey(key))
-            return values[hashFun(key)];
-        return null;
+        OptionalInt foundKeyOpt = Arrays.stream(getKeySearchIndexSequence(key, hashFun(key)))
+                .filter(i -> slots[i] != null && slots[i].equals(key))
+                .findFirst();
+        return foundKeyOpt.isPresent() ? values[foundKeyOpt.getAsInt()] : null;
+    }
+
+    private int[] getKeySearchIndexSequence(String key, int index) {
+        return IntStream.concat(IntStream.range(index, size), IntStream.range(0, index)).toArray();
+    }
+
+    class PutConsumer implements IntConsumer {
+        private final String[] slots;
+        private final T[] values;
+        private final String key;
+        private final T value;
+
+        public PutConsumer(String[] slots, T[] values, String key, T value) {
+            this.slots = slots;
+            this.values = values;
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public void accept(int value) {
+            this.slots[value] = this.key;
+            this.values[value] = this.value;
+        }
     }
 }
---
+++ src/test/java/NativeDictionaryTest.java
@@ -0,0 +1,60 @@
+import data_structures.native_dictionary.NativeDictionary;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class NativeDictionaryTest {
+    @Test
+    public void isKey_When_notContains() {
+        NativeDictionary<String> nativeDictionary = new NativeDictionary<>(10, String.class);
+
+        Assertions.assertThat(nativeDictionary.isKey("1")).isFalse();
+    }
+
+    @Test
+    public void isKey_When_contains() {
+        NativeDictionary<String> nativeDictionary = new NativeDictionary<>(10, String.class);
+
+        nativeDictionary.put("1", "123");
+
+        Assertions.assertThat(nativeDictionary.isKey("1")).isTrue();
+    }
+
+    @Test
+    public void isKey_When_containsAndCollision() {
+        NativeDictionary<String> nativeDictionary = new NativeDictionary<>(2, String.class);
+
+        nativeDictionary.put("1", "123");
+        nativeDictionary.put("3", "123");
+
+        Assertions.assertThat(nativeDictionary.isKey("1")).isTrue();
+        Assertions.assertThat(nativeDictionary.isKey("3")).isTrue();
+    }
+
+    @Test
+    public void put_When_empty() {
+        NativeDictionary<String> nativeDictionary = new NativeDictionary<>(2, String.class);
+
+        String expectedKey = "1";
+        String expectedValue = "123";
+        nativeDictionary.put(expectedKey, expectedValue);
+
+        Assertions.assertThat(nativeDictionary.slots[1]).isEqualTo(expectedKey);
+        Assertions.assertThat(nativeDictionary.values[1]).isEqualTo(expectedValue);
+    }
+
+    @Test
+    public void put_When_collision() {
+        NativeDictionary<String> nativeDictionary = new NativeDictionary<>(2, String.class);
+
+        String expectedKey = "1";
+        String collisionExpectedKey = "3";
+        String expectedValue = "123";
+        nativeDictionary.put(expectedKey, expectedValue);
+        nativeDictionary.put(collisionExpectedKey, expectedValue);
+
+        Assertions.assertThat(nativeDictionary.values[0]).isEqualTo(expectedValue);
+        Assertions.assertThat(nativeDictionary.slots[0]).isEqualTo(collisionExpectedKey);
+        Assertions.assertThat(nativeDictionary.slots[1]).isEqualTo(expectedKey);
+        Assertions.assertThat(nativeDictionary.values[1]).isEqualTo(expectedValue);
+    }
+}
