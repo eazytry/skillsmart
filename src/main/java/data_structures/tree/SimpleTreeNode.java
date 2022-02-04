8a395c04c7e68659393f99a8c1b30088419be7bb
Алексей Фомичев <agfomichev@alfabank.ru>
2022-02-04 21:16:11 +0300

    add SimpleTreeNode && tests

---
+++ src/main/java/data_structures/tree/SimpleTreeNode.java
@@ -0,0 +1,91 @@
+package data_structures.tree;
+
+import java.util.*;
+
+public class SimpleTreeNode<T> {
+    public T NodeValue; // значение в узле
+    public SimpleTreeNode<T> Parent; // родитель или null для корня
+    public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+
+    public SimpleTreeNode(T val, SimpleTreeNode<T> parent) {
+        NodeValue = val;
+        Parent = parent;
+        Children = new ArrayList<>();
+    }
+}
+
+class SimpleTree<T> {
+    public SimpleTreeNode<T> root; // корень, может быть null
+
+    public SimpleTree(SimpleTreeNode<T> root) {
+        this.root = root;
+    }
+
+    public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild) {
+        NewChild.Parent = ParentNode;
+        ParentNode.Children.add(NewChild);
+    }
+
+    public void DeleteNode(SimpleTreeNode<T> NodeToDelete) {
+        NodeToDelete.Parent.Children.remove(NodeToDelete);
+    }
+
+    public List<SimpleTreeNode<T>> GetAllNodes() {
+        return getSubNodes(root);
+    }
+
+    private List<SimpleTreeNode<T>> getSubNodes(SimpleTreeNode<T> node) {
+        List<SimpleTreeNode<T>> nodes = new ArrayList<>();
+        nodes.add(node);
+        if (node.Children == null || node.Children.isEmpty()) {
+            return nodes;
+        }
+        for (SimpleTreeNode<T> subNode : node.Children) {
+            nodes.addAll(getSubNodes(subNode));
+        }
+        return nodes;
+    }
+
+    public List<SimpleTreeNode<T>> FindNodesByValue(T val) {
+        return findNodesByValue(root, val);
+    }
+
+    private List<SimpleTreeNode<T>> findNodesByValue(SimpleTreeNode<T> node, T val) {
+        List<SimpleTreeNode<T>> nodes = new ArrayList<>();
+        if (node.NodeValue != null && node.NodeValue.equals(val)) {
+            nodes.add(node);
+        }
+        if (node.Children == null || node.Children.isEmpty()) {
+            return nodes;
+        }
+        for (SimpleTreeNode<T> subNode : node.Children) {
+            nodes.addAll(findNodesByValue(subNode, val));
+        }
+        return nodes;
+    }
+
+    public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent) {
+        NewParent.Children.add(OriginalNode);
+        OriginalNode.Parent = NewParent;
+    }
+
+    public int Count() {
+        return GetAllNodes().size();
+    }
+
+    public int LeafCount() {
+        return findLeafs(root).size();
+    }
+
+    private List<SimpleTreeNode<T>> findLeafs(SimpleTreeNode<T> node) {
+        List<SimpleTreeNode<T>> nodes = new ArrayList<>();
+        if (node.Children == null || node.Children.isEmpty()) {
+            nodes.add(node);
+            return nodes;
+        }
+        for (SimpleTreeNode<T> subNode : node.Children) {
+            nodes.addAll(findLeafs(subNode));
+        }
+        return nodes;
+    }
+}
---
+++ src/test/java/data_structures/tree/SimpleTreeTest.java
@@ -0,0 +1,73 @@
+package data_structures.tree;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleTreeTest {
+        @Test
+        public void getAllWhenFull() {
+            //when
+            var root = new SimpleTreeNode<>("123", null);
+            var second = new SimpleTreeNode<>("321", null);
+            var third = new SimpleTreeNode<>("321", null);
+            var fourth = new SimpleTreeNode<>("321", null);
+            var fifth = new SimpleTreeNode<>("213213", null);
+            var stringSimpleTree = new SimpleTree<>(root);
+            stringSimpleTree.AddChild(root, second);
+            stringSimpleTree.AddChild(root, third);
+            stringSimpleTree.AddChild(second, fourth);
+            stringSimpleTree.AddChild(third, fifth);
+
+            var nodes = stringSimpleTree.GetAllNodes();
+
+
+            Assertions.assertTrue(nodes.contains(root));
+            Assertions.assertTrue(nodes.contains(second));
+            Assertions.assertTrue(nodes.contains(third));
+            Assertions.assertTrue(nodes.contains(fourth));
+            Assertions.assertTrue(nodes.contains(fifth));
+        }
+
+        @Test
+        public void findByValueWhenFull() {
+            //when
+            var root = new SimpleTreeNode<>("123", null);
+            var second = new SimpleTreeNode<>("321", null);
+            var third = new SimpleTreeNode<>("321", null);
+            var fourth = new SimpleTreeNode<>("321", null);
+            var fifth = new SimpleTreeNode<>("213213", null);
+            var stringSimpleTree = new SimpleTree<>(root);
+            stringSimpleTree.AddChild(root, second);
+            stringSimpleTree.AddChild(root, third);
+            stringSimpleTree.AddChild(second, fourth);
+            stringSimpleTree.AddChild(third, fifth);
+
+            var nodes = stringSimpleTree.FindNodesByValue("321");
+
+            Assertions.assertTrue(nodes.contains(second));
+            Assertions.assertTrue(nodes.contains(third));
+            Assertions.assertTrue(nodes.contains(fourth));
+        }
+
+        @Test
+        public void findLeafsWhenFull() {
+            //when
+            var root = new SimpleTreeNode<>("123", null);
+            var second = new SimpleTreeNode<>("321", null);
+            var third = new SimpleTreeNode<>("321", null);
+            var fourth = new SimpleTreeNode<>("321", null);
+            var fifth = new SimpleTreeNode<>("213213", null);
+            var stringSimpleTree = new SimpleTree<>(root);
+            stringSimpleTree.AddChild(root, second);
+            stringSimpleTree.AddChild(root, third);
+            stringSimpleTree.AddChild(second, fourth);
+            stringSimpleTree.AddChild(third, fifth);
+
+            var count = stringSimpleTree.LeafCount();
+
+            Assertions.assertEquals(2, count);
+        }
+}
