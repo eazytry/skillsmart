8f73b10efd81e7eebd8479ed02d9572ca2fb5b2a
Алексей Фомичев <agfomichev@alfabank.ru>
2022-03-22 23:31:20 +0300

    add BalancedBST && test

---
+++ src/main/java/data_structures/balanced_bst/BSTNode.java
@@ -0,0 +1,66 @@
+package data_structures.balanced_bst;
+
+import java.util.Arrays;
+
+class BSTNode {
+    public int NodeKey; // ключ узла
+    public BSTNode Parent; // родитель или null для корня
+    public BSTNode LeftChild; // левый потомок
+    public BSTNode RightChild; // правый потомок	
+    public int Level; // глубина узла
+
+    public BSTNode(int key, BSTNode parent) {
+        NodeKey = key;
+        Parent = parent;
+        LeftChild = null;
+        RightChild = null;
+    }
+}
+
+class BalancedBST {
+    public BSTNode Root; // корень дерева
+
+    public BalancedBST() {
+        Root = null;
+    }
+
+    public void GenerateTree(int[] a) {
+        Arrays.sort(a);
+
+        findNode(a, null, 0);
+        // создаём дерево с нуля из неотсортированного массива a
+        // ...
+    }
+
+    public BSTNode findNode(int[] a, BSTNode parent, int level) {
+        if (a.length == 0) {
+            return null;
+        }
+        var currentKeyIndex = a.length / 2;
+        BSTNode current = new BSTNode(a[currentKeyIndex], parent);
+        current.Level = level;
+        if (this.Root == null) {
+            this.Root = current;
+        }
+        if (a.length > 1) {
+            current.LeftChild = findNode(Arrays.copyOfRange(a, 0, currentKeyIndex), current, level + 1);
+            current.RightChild = findNode(Arrays.copyOfRange(a, currentKeyIndex + 1, a.length), current, level + 1);
+        }
+
+        return current;
+    }
+
+    public boolean IsBalanced(BSTNode root_node) {
+        if (root_node == null) {
+            return true;
+        }
+        if (root_node.LeftChild == null && root_node.RightChild == null && root_node.Level > 1) {
+            return root_node.Parent.Parent.LeftChild != null && root_node.Parent.Parent.RightChild != null;
+        }
+        var isLeftSubtreeBalanced = IsBalanced(root_node.LeftChild);
+        var isRightSubtreeBalanced = IsBalanced(root_node.RightChild);
+
+
+        return isLeftSubtreeBalanced && isRightSubtreeBalanced; // сбалансировано ли дерево с корнем root_node
+    }
+} 
---
+++ src/test/java/data_structures/balanced_bst/BalancedBSTTest.java
@@ -0,0 +1,101 @@
+package data_structures.balanced_bst;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BalancedBSTTest {
+    @Test
+    public void whenEmpty_shouldCreate() {
+        var balancedBST = new BalancedBST();
+
+        balancedBST.GenerateTree(new int[]{});
+
+        Assertions.assertNull(balancedBST.Root);
+    }
+
+    @Test
+    public void whenArraySizeIsOne_shouldCreate() {
+        var balancedBST = new BalancedBST();
+
+        balancedBST.GenerateTree(new int[]{1});
+
+        Assertions.assertEquals(1, balancedBST.Root.NodeKey);
+        Assertions.assertEquals(0, balancedBST.Root.Level);
+        Assertions.assertNull(balancedBST.Root.LeftChild);
+        Assertions.assertNull(balancedBST.Root.RightChild);
+    }
+
+    @Test
+    public void whenArraySizeIsTwo_shouldCreate() {
+        var balancedBST = new BalancedBST();
+
+        balancedBST.GenerateTree(new int[]{1, 2});
+
+        Assertions.assertEquals(2, balancedBST.Root.NodeKey);
+        Assertions.assertEquals(0, balancedBST.Root.Level);
+        Assertions.assertNotNull(balancedBST.Root.LeftChild);
+        Assertions.assertNull(balancedBST.Root.RightChild);
+        Assertions.assertEquals(1, balancedBST.Root.LeftChild.NodeKey);
+        Assertions.assertEquals(1, balancedBST.Root.LeftChild.Level);
+    }
+
+    @Test
+    public void whenArraySizeIsThree_shouldCreate() {
+        var balancedBST = new BalancedBST();
+
+        balancedBST.GenerateTree(new int[]{1, 2, 3});
+
+        Assertions.assertEquals(2, balancedBST.Root.NodeKey);
+        Assertions.assertEquals(0, balancedBST.Root.Level);
+        Assertions.assertNull(balancedBST.Root.Parent);
+        Assertions.assertNotNull(balancedBST.Root.LeftChild);
+        Assertions.assertNotNull(balancedBST.Root.RightChild);
+        Assertions.assertEquals(1, balancedBST.Root.LeftChild.NodeKey);
+        Assertions.assertEquals(1, balancedBST.Root.LeftChild.Level);
+        Assertions.assertEquals(balancedBST.Root, balancedBST.Root.LeftChild.Parent);
+        Assertions.assertNull(balancedBST.Root.LeftChild.LeftChild);
+        Assertions.assertNull(balancedBST.Root.LeftChild.RightChild);
+        Assertions.assertEquals(3, balancedBST.Root.RightChild.NodeKey);
+        Assertions.assertEquals(1, balancedBST.Root.RightChild.Level);
+        Assertions.assertEquals(balancedBST.Root, balancedBST.Root.RightChild.Parent);
+        Assertions.assertNull(balancedBST.Root.RightChild.LeftChild);
+        Assertions.assertNull(balancedBST.Root.RightChild.RightChild);
+    }
+
+    @Test
+    public void whenIsBalancedShouldTrueWhenOneElement() {
+        var bst = new BalancedBST();
+        bst.GenerateTree(new int[]{1});
+
+        Assertions.assertTrue(bst.IsBalanced(bst.Root));
+    }
+
+    @Test
+    public void whenIsBalancedShouldTrueWhenTwoElements() {
+        var bst = new BalancedBST();
+        bst.GenerateTree(new int[]{1, 2});
+
+        Assertions.assertTrue(bst.IsBalanced(bst.Root));
+    }
+
+    @Test
+    public void whenIsBalancedShouldTrueWhenTreeElements() {
+        var bst = new BalancedBST();
+        bst.GenerateTree(new int[]{1, 2, 3});
+
+        Assertions.assertTrue(bst.IsBalanced(bst.Root));
+    }
+
+    @Test
+    public void whenIsBalancedShouldFalseWhenTreeElements() {
+        var bst = new BalancedBST();
+        bst.Root = new BSTNode(3, null);
+        bst.Root.Level = 0;
+        bst.Root.LeftChild = new BSTNode(2, bst.Root);
+        bst.Root.LeftChild.Level = 1;
+        bst.Root.LeftChild.LeftChild = new BSTNode(1, bst.Root.LeftChild);
+        bst.Root.LeftChild.LeftChild.Level = 2;
+
+        Assertions.assertFalse(bst.IsBalanced(bst.Root));
+    }
+}
