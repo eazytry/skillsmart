c4153231f19de9179ed17f224c5170edc4b7538b
Алексей Фомичев <agfomichev@alfabank.ru>
2022-03-31 23:39:55 +0300

    add depth search for graph

--- src/main/java/data_structures/graph/Vertex.java
+++ src/main/java/data_structures/graph/Vertex.java
@@ -1,12 +1,15 @@
 package data_structures.graph;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Vertex {
     public int Value;
+    public boolean Hit;
 
     public Vertex(int val) {
         Value = val;
+        Hit = false;
     }
 }
 
@@ -66,5 +69,50 @@ class SimpleGraph {
         m_adjacency[v2][v1] = 0;
         m_adjacency[v1][v2] = 0;
         // удаление ребра между вершинами v1 и v2
+    }
+
+    public ArrayList<Vertex> DepthFirstSearch(int VFrom, int VTo) {
+        if (max_vertex == 0) {
+            return new ArrayList<>();
+        }
+        clearHits();
+        Stack<Integer> stack = new Stack<>();
+        vertex[VFrom].Hit = true;
+        stack.push(VFrom);
+
+        return DepthFirstSearch(stack, VTo).stream().map(index -> vertex[index]).collect(Collectors.toCollection(ArrayList::new));
+        // Узлы задаются позициями в списке vertex.
+        // Возвращается список узлов -- путь из VFrom в VTo.
+        // Список пустой, если пути нету.
+    }
+
+    private void clearHits() {
+        Arrays.stream(vertex).forEach(v -> v.Hit = false);
+    }
+
+    private Stack<Integer> DepthFirstSearch(Stack<Integer> stack, int VTo) {
+        if (stack.isEmpty()) {
+            return stack;
+        }
+        var foundVertex = searchLinkedNotHitVertexOrToIndex(stack.peek());
+        if (foundVertex == null) {
+            stack.pop();
+        } else {
+            vertex[foundVertex].Hit = true;
+            stack.push(foundVertex);
+            if (foundVertex == VTo) {
+                return stack;
+            }
+        }
+        return DepthFirstSearch(stack, VTo);
+    }
+
+    private Integer searchLinkedNotHitVertexOrToIndex(int index) {
+        for (int i = 0; i < max_vertex; i++) {
+            if ((m_adjacency[i][index] > 0 || m_adjacency[index][i] > 0) && !vertex[i].Hit) {
+                return i;
+            }
+        }
+        return null;
     }
 }
--- src/test/java/data_structures/graph/GraphTest.java
+++ src/test/java/data_structures/graph/GraphTest.java
@@ -3,6 +3,8 @@ package data_structures.graph;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.ArrayList;
+
 public class GraphTest {
     @Test
     public void testAddVertexWhenEmpty() {
@@ -54,6 +56,58 @@ public class GraphTest {
 
         Assertions.assertNull(graph.vertex[2]);
         Assertions.assertEquals(0, graph.m_adjacency[0][2]);
-        Assertions.assertEquals(0 ,graph.m_adjacency[1][2]);
+        Assertions.assertEquals(0, graph.m_adjacency[1][2]);
+    }
+
+    @Test
+    public void testDepthFirstSearchWhenEmpty() {
+        var graph = new SimpleGraph(0);
+
+        var vertices = graph.DepthFirstSearch(0, 0);
+
+        Assertions.assertTrue(vertices.isEmpty());
+    }
+
+    @Test
+    public void testDepthFirstSearch() {
+        var graph = new SimpleGraph(4);
+
+        // add vertexes
+        graph.vertex[0] = new Vertex(1);
+        graph.vertex[1] = new Vertex(2);
+        graph.vertex[2] = new Vertex(3);
+        graph.vertex[3] = new Vertex(4);
+
+        // add edges
+        graph.m_adjacency[0][2] = 1;
+        graph.m_adjacency[2][1] = 1;
+        graph.m_adjacency[1][3] = 1;
+
+        var vertices = graph.DepthFirstSearch(0, 3);
+
+        Assertions.assertFalse(vertices.isEmpty());
+        Assertions.assertEquals(1, vertices.get(0).Value);
+        Assertions.assertEquals(3, vertices.get(1).Value);
+        Assertions.assertEquals(2, vertices.get(2).Value);
+        Assertions.assertEquals(4, vertices.get(3).Value);
+    }
+
+    @Test
+    public void testDepthFirstSearchNotFound() {
+        var graph = new SimpleGraph(4);
+
+        // add vertexes
+        graph.vertex[0] = new Vertex(1);
+        graph.vertex[1] = new Vertex(2);
+        graph.vertex[2] = new Vertex(3);
+        graph.vertex[3] = new Vertex(4);
+
+        // add edges
+        graph.m_adjacency[0][2] = 1;
+        graph.m_adjacency[1][3] = 1;
+
+        var vertices = graph.DepthFirstSearch(0, 3);
+
+        Assertions.assertTrue(vertices.isEmpty());
     }
 }
