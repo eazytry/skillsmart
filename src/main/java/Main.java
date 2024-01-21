0086b296afb0f92815a28402786394d32ea740d8
Alexey Fomichev <egida5629@gmail.com>
2024-01-21 21:59:32 +0300

    add ooap2 third task solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,5 @@
 import clear_code.FactoryMethodExample;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.Supplier;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println(FactoryMethodExample.fromLocal().getTimeOffset());
