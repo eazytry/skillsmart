6698f571ca2c49acdf88dca0b1a13ba0157aeb25
Alexey Fomichev <egida5629@gmail.com>
2024-02-24 00:51:52 +0300

    fiveteenth ooap2 solution

---
+++ src/main/java/ooap_second/task_fiveteenth/Solution.java
@@ -0,0 +1,33 @@
+package ooap_second.task_fiveteenth;
+
+public class Solution {
+    static abstract class LoanApplication {
+        // проскорить заявку на ипотеку
+        abstract boolean score();
+        // получить данные по доходам в формате json
+        String getIncomeData() {
+            // Допустим тут идем в ESIA по ФИО/паспортным данны и получаем (общая реализация
+            return null;
+        }
+    }
+
+    // Зарплатный клиент
+    static class StaffLoanApplication extends LoanApplication{
+        // для него кредит всегда выдаем
+        @Override
+        boolean score() {
+            return true;
+        }
+    }
+
+    // Клиент с улицы
+    static class StreetLoanApplication extends LoanApplication {
+
+        @Override
+        boolean score() {
+            var incomeData = super.getIncomeData();
+            // какие-то действия и анализ со справками по доходу
+            return false;
+        }
+    }
+}
